550e3004a4ee4bdb2c461b0285b3cefd0fdbbaa7
Jeffrey Brian Mahler <jmahler@imail.EECS.Berkeley.EDU>
2017-03-01 19:57:21 -0800

    Made opencv and tensorflow optional (doesn't break install)

--- setup.py
+++ setup.py
@@ -8,14 +8,10 @@ requirements = [
     'numpy',
     'scipy',
     'matplotlib',
-    'opencv-python',
     'cycler',
-    'tensorflow',
     'Pillow',
     'core'
 ]
-
-dependency_links=['https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.11.0-cp27-none-linux_x86_64.whl']
 
 setup(name='perception',
       version='0.1.dev0',
@@ -25,6 +21,5 @@ setup(name='perception',
       package_dir = {'': '.'},
       packages=['perception'],
       install_requires=requirements,
-      dependency_links=dependency_links,
       test_suite='test'
      )
