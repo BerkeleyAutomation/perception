50d6395993b09a2d48cb63c416b14d06f69e7734
BinaryHalibut <alanpusongli@gmail.com>
2017-03-09 19:38:06 -0800

    add framerate counter, fix dtype passing

--- perception/ros_image_buffer.py
+++ perception/ros_image_buffer.py
@@ -17,6 +17,8 @@ if __name__ == '__main__':
                         help="Make the path to the ROS stream absolute (do not prepend ROS_NAMESPACE)")
     parser.add_argument('--bufsize', dest='bufsize', default=100, type=int,
                         help="Specify the number of images to buffer (default 100)")
+    parser.add_argument('-r', '--show_framerate', dest='show_framerate', action='store_true',
+                        help="Show a framerate message every 10 seconds")
     args = parser.parse_args()
     
     bufsize = args.bufsize
@@ -28,14 +30,33 @@ if __name__ == '__main__':
     
     bridge = CvBridge()
     buffer_of_images = []
+    if args.show_framerate:
+        dtype = 'float32'
+        images_so_far = 0
+        time_old = rospy.get_rostime()
+        ten_secs = rospy.Duration(10, 0)
     def callback(data):
+        global dtype
+        if args.show_framerate:
+            global images_so_far
+            global time_old
+            global ten_secs
         try:
             cv_image = bridge.imgmsg_to_cv2(data)
         except CvBridgeError as e:
             print(e)
+        dtype = str(cv_image.dtype)
         buffer_of_images.insert(0, np.asarray(cv_image, dtype='float32'))
         if(len(buffer_of_images) > bufsize):
             buffer_of_images.pop()
+        
+        if args.show_framerate:
+            images_so_far += 1
+            time_new = rospy.get_rostime()
+            if time_new - time_old >= ten_secs:
+                print("{0} frames recorded in the past 10 seconds".format(images_so_far))
+                time_old = time_new
+                images_so_far = 0
 
     rospy.Subscriber(stream_to_buffer, Image, callback)
     
@@ -45,7 +66,7 @@ if __name__ == '__main__':
         images_per_frame = 1 if len(image_shape) == 2 else image_shape[2]
         
         ret = np.dstack(to_return)
-        return ImageBufferResponse(images_per_frame, str(ret.dtype), ret.ravel(), *ret.shape)
+        return ImageBufferResponse(images_per_frame, dtype, ret.ravel(), *ret.shape)
     
     s = rospy.Service('stream_image_buffer', ImageBuffer, handle_request)
     
