3e9e0f271d7a346a4d24378c85fccaaa9a7b83fd
Jacky Liang <jackyliang@berkeley.edu>
2017-03-12 15:42:45 -0700

    adding flush to camera sensor

--- perception/opencv_camera_sensor.py
+++ perception/opencv_camera_sensor.py
@@ -20,18 +20,24 @@ class OpenCVCameraSensor(CameraSensor):
         self._sensor = VideoCapture(self._device_id)
         if not self._sensor.isOpened():
             raise Exception("Unable to open OpenCVCameraSensor for id {0}".format(self._device_id))
-        for _ in range(4):
+        self.flush()
+
+    def flush(self):
+        start = time()
+        for _ in range(6):
             self._sensor.read()
+        print 'took {}s per frame'.format((time() - start)/6)
 
     def stop(self):
         """ Stops the OpenCVCameraSensor Stream """
         self._sensor.release()
 
-    def frames(self):
+    def frames(self, flush=True):
         """ Returns the latest color image from the stream
         Raises:
             Exception if opencv sensor gives ret_val of 0
         """
+        self.flush()
         ret_val, frame = self._sensor.read()
         if not ret_val:
             raise Exception("Unable to retrieve frame from OpenCVCameraSensor for id {0}".format(self._device_id))
