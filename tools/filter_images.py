3d1d199135d84d2fdd80753a90ec8df7c52277c5
jeff <jmahler@berkeley.edu>
2018-02-14 16:26:41 -0800

    Updated filtering script

--- tools/filter_images.py
+++ tools/filter_images.py
@@ -13,18 +13,18 @@ import rospy
 import sys
 import time
 
-from autolab_core import PointCloud, RigidTransform
+from autolab_core import Box, PointCloud, RigidTransform
 from perception import VirtualSensor
 from visualization import Visualizer2D as vis2d
 from visualization import Visualizer3D as vis3d
 
 min_height = 0.001
 max_height = 0.15
-rescale_factor = 0.25
+rescale_factor = 0.5
 
 vis_clipping = False
 vis_segments = False
-vis_final_clouds = True
+vis_final_clouds = False
 vis_final_images = True
 
 if __name__ == '__main__':
@@ -51,19 +51,26 @@ if __name__ == '__main__':
     color_im, depth_im, _ = sensor.frames()
 
     # inpaint original image
+    depth_im_filtered = depth_im.copy()
     depth_im_orig = depth_im.inpaint(rescale_factor)
+
+    # timing
+    filter_start = time.time()
     
-    # convert to point cloud in world coords
     small_depth_im = depth_im.resize(rescale_factor, interp='nearest')
     small_camera_intr = camera_intr.resize(rescale_factor)
 
+    # convert to point cloud in world coords
+    deproject_start = time.time()
     point_cloud_cam = small_camera_intr.deproject(small_depth_im)
     point_cloud_cam.remove_zero_points()
     point_cloud_world = T_camera_world * point_cloud_cam
 
     point_cloud_filtered = copy.deepcopy(point_cloud_world)
-
+    logging.info('Deproject took %.3f sec' %(time.time()-deproject_start))
+    
     # filter low
+    clip_start = time.time()
     low_indices = np.where(point_cloud_world.data[2,:] < min_height)[0] 
     point_cloud_filtered.data[2,low_indices] = min_height 
 
@@ -71,6 +78,11 @@ if __name__ == '__main__':
     high_indices = np.where(point_cloud_world.data[2,:] > max_height)[0] 
     point_cloud_filtered.data[2,high_indices] = max_height 
 
+    # re-project and update depth im
+    #depth_im_filtered = camera_intr.project_to_image(T_camera_world.inverse() * point_cloud_filtered)
+    logging.info('Clipping took %.3f sec' %(time.time()-clip_start))
+    
+    # vis
     focal_point = np.mean(point_cloud_filtered.data,
                           axis=1)
     if vis_clipping:
@@ -87,9 +99,18 @@ if __name__ == '__main__':
                      subsample=10)
         vis3d.show()
     
-    # PCL filters
     pcl_start = time.time()
-    pcl_cloud = pcl.PointCloud(point_cloud_filtered.data.T.astype(np.float32))
+
+    # subsample point cloud
+    #rate = int(1.0 / rescale_factor)**2
+    #point_cloud_filtered = point_cloud_filtered.subsample(rate, random=False)
+    box = Box(np.array([0.2, -0.24, min_height]), np.array([0.56, 0.21, max_height]), frame='world')
+    point_cloud_masked, valid_indices = point_cloud_filtered.box_mask(box)
+    invalid_indices = np.setdiff1d(np.arange(point_cloud_filtered.num_points),
+                                   valid_indices)
+    
+    # apply PCL filters
+    pcl_cloud = pcl.PointCloud(point_cloud_masked.data.T.astype(np.float32))
     tree = pcl_cloud.make_kdtree()
     ec = pcl_cloud.make_EuclideanClusterExtraction()
     ec.set_ClusterTolerance(0.005)
@@ -102,7 +123,7 @@ if __name__ == '__main__':
     num_clusters = len(cluster_indices)
 
     segments = []
-    filtered_points = np.zeros([3,point_cloud_filtered.num_points])
+    filtered_points = np.zeros([3,point_cloud_masked.num_points])
     cur_i = 0
     for j, indices in enumerate(cluster_indices):
         num_points = len(indices)
@@ -119,8 +140,8 @@ if __name__ == '__main__':
         seg_point_cloud = PointCloud(points, frame='world')
         segments.append(seg_point_cloud)
 
-    
-    point_cloud_filtered = PointCloud(filtered_points[:,:cur_i], frame='world')        
+    all_points = np.c_[filtered_points[:,:cur_i], point_cloud_filtered.data[:,invalid_indices]]
+    point_cloud_filtered = PointCloud(all_points, frame='world')        
     pcl_stop = time.time()
     logging.info('PCL Seg took %.3f sec' %(pcl_stop-pcl_start))
         
@@ -151,20 +172,35 @@ if __name__ == '__main__':
         vis3d.show()
         
     # convert to depth image
+    project_start = time.time()
     point_cloud_cam = T_camera_world.inverse() * point_cloud_filtered
-    depth_im_filtered = small_camera_intr.project_to_image(point_cloud_cam)
-    depth_im_filtered = depth_im_filtered.inpaint()#rescale_factor)
-    depth_im_filtered = depth_im_filtered.resize(1.0/rescale_factor)
+    #depth_im_noise = small_camera_intr.project_to_image(point_cloud_cam)
+    depth_im_filtered = small_camera_intr.project_to_image(point_cloud_cam)    
+    #depth_im_filtered = depth_im_filtered.resize(1.0/rescale_factor)#, interp='nearest')
+    noise_mask = depth_im_filtered.to_binary()
+    #depth_im_filtered = depth_im_filtered.inpaint(rescale_factor)
+    #depth_im_noise = depth_im_noise.resize(1.0/rescale_factor)
+    #noise_mask = depth_im_noise.to_binary()
+    logging.info('Project took %.3f sec' %(time.time() - project_start))
+    #depth_im_filtered = depth_im.mask_binary(noise_mask)
+    #depth_im_filtered = depth_im_filtered.mask_binary(noise_mask.inverse())
+    depth_im_filtered = depth_im_filtered.inpaint(0.5)
+    
+    filter_stop = time.time()
+    logging.info('Filtering took %.3f sec' %(filter_stop-filter_start))
     
     if vis_final_images:
         vis2d.figure()
-        vis2d.subplot(1,3,1)
+        vis2d.subplot(2,2,1)
         vis2d.imshow(depth_im)
         vis2d.title('Orig')
-        vis2d.subplot(1,3,2)
+        vis2d.subplot(2,2,2)
         vis2d.imshow(depth_im_orig)
         vis2d.title('Inpainted')
-        vis2d.subplot(1,3,3)
+        vis2d.subplot(2,2,3)
+        vis2d.imshow(noise_mask)
+        vis2d.title('Mask')
+        vis2d.subplot(2,2,4)
         vis2d.imshow(depth_im_filtered)
         vis2d.title('Filtered')
         vis2d.show()
